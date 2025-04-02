c7a6ccc393ba4ee8cc3fc0322d117a4c64f46620
Andy Baxter <andrewbaxter439@gmail.com>
2025-04-02 15:01:48 +0100

    simpler tests

--- src/test/java/simpaths/data/filters/EmploymentHistoryFilterTest.java
+++ src/test/java/simpaths/data/filters/EmploymentHistoryFilterTest.java
@@ -1,15 +1,9 @@
 package simpaths.data.filters;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import simpaths.model.Person;
 import simpaths.model.enums.Les_c4;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,69 +19,25 @@ class EmploymentHistoryFilterTest {
         return testPerson;
     }
 
-    public static class PersonTestData {
-        final Les_c4 les_c4_lag1;
-        final boolean isFilteredEmployed;
-        final boolean isFilteredUnemployed;
-
-        PersonTestData(Les_c4 les_c4_lag1, boolean isFilteredEmployed, boolean isFilteredUnemployed) {
-            this.les_c4_lag1 = les_c4_lag1;
-            this.isFilteredEmployed = isFilteredEmployed;
-            this.isFilteredUnemployed = isFilteredUnemployed;
-        }
+    @Test
+    @DisplayName("Employed filter only returns true for employed or self-employed persons")
+    void employedOrSelfEmployed() {
+        EmploymentHistoryFilter filter = new EmploymentHistoryFilter(Les_c4.EmployedOrSelfEmployed);
+        assertTrue(filter.isFiltered(createTestPerson(Les_c4.EmployedOrSelfEmployed)));
+        assertFalse(filter.isFiltered(createTestPerson(Les_c4.NotEmployed)));
+        assertFalse(filter.isFiltered(createTestPerson(Les_c4.Student)));
+        assertFalse(filter.isFiltered(createTestPerson(Les_c4.Retired)));
     }
 
-    private static final List<PersonTestData> TEST_SCENARIOS = Arrays.asList(
-                // Eight with employed history
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                new PersonTestData(Les_c4.EmployedOrSelfEmployed, true, false),
-                // Four with not employed history
-                new PersonTestData(Les_c4.NotEmployed, false, true),
-                new PersonTestData(Les_c4.NotEmployed, false, true),
-                new PersonTestData(Les_c4.NotEmployed, false, true),
-                new PersonTestData(Les_c4.NotEmployed, false, true),
-                // Ignore all rest as should be filtered out
-                new PersonTestData(Les_c4.Student, false, false),
-                new PersonTestData(Les_c4.Retired, false, false),
-                new PersonTestData(Les_c4.Retired, false, false),
-                new PersonTestData(Les_c4.Student, false, false)
-    );
-
-
-    @ParameterizedTest
-    @MethodSource("getPreviousEmploymentScenarios")
-    @DisplayName("Testing filtering previously employed")
-    public void populationPreviouslyEmployed(PersonTestData testData) {
-
-        Person testPerson = createTestPerson(testData.les_c4_lag1);
-
-        EmploymentHistoryFilter employmentHistoryUnemployed = new EmploymentHistoryFilter(Les_c4.EmployedOrSelfEmployed);
-
-        assertEquals(testData.isFilteredEmployed, employmentHistoryUnemployed.isFiltered(testPerson));
-
+    @Test
+    @DisplayName("Unemployed filter only returns true for unemployed persons")
+    void unEmployed() {
+        EmploymentHistoryFilter filter = new EmploymentHistoryFilter(Les_c4.NotEmployed);
+        assertFalse(filter.isFiltered(createTestPerson(Les_c4.EmployedOrSelfEmployed)));
+        assertTrue(filter.isFiltered(createTestPerson(Les_c4.NotEmployed)));
+        assertFalse(filter.isFiltered(createTestPerson(Les_c4.Student)));
+        assertFalse(filter.isFiltered(createTestPerson(Les_c4.Retired)));
     }
 
-    @ParameterizedTest
-    @MethodSource("getPreviousEmploymentScenarios")
-    @DisplayName("Testing filtering previously unemployed")
-    public void populationPreviouslyUnemployed(PersonTestData testData) {
-
-        Person testPerson = createTestPerson(testData.les_c4_lag1);
-
-        EmploymentHistoryFilter employmentHistoryUnemployed = new EmploymentHistoryFilter(Les_c4.NotEmployed);
-
-        assertEquals(testData.isFilteredUnemployed, employmentHistoryUnemployed.isFiltered(testPerson));
-    }
-
-    private static Stream<Arguments> getPreviousEmploymentScenarios() {
-        return TEST_SCENARIOS.stream()
-                .map(Arguments::of);
-    }
 
 }
